a8d67801f366c9dd9c32a4fc24d9a0178b961d56
Patrick Birchall <pbirchall900@gmail.com>
2017-08-24 12:17:18 +0100

    removing redundant volume blocks

--- servers.tf
+++ servers.tf
@@ -7,9 +7,4 @@ resource "scaleway_server" "blog_server" {
   image = "${data.scaleway_image.ubuntu.id}"
   name = "blog_server"
   type = "${var.server_type}"
-
-  volume {
-    size_in_gb = 20
-    type = "l_ssd"
-  }
 }
